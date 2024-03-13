f3cccc60509b67d656b59e8155ff815368f1b380
Federico Marquez <83078166+fedemarquez0@users.noreply.github.com>
2024-03-13 10:35:02 -0300

    Agregando Repositorio

---
+++ ventas/encabezado.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Final LAB-II</title>
+	<link href="imagenes/logo.ico" rel="icon" type="image/x-icon" />
+	
+	<link rel="stylesheet" href="./css/all.min.css">
+	<link rel="stylesheet" href="./css/2.css">
+	<link rel="stylesheet" href="./css/estilo.css">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="logo" href="#inicio"><img src="./imagenes/logo-ubp.png" alt="Facultad logotipo" style="max-width:80%;width:auto;height:auto;"></a>
+				<!---<a class="navbar-brand" href="#">POS</a>--->
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="./listar.php">Productos</a></li>
+					<li><a href="./vender.php">Vender</a></li>
+					<li><a href="./ventas.php">Ventas</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row">
